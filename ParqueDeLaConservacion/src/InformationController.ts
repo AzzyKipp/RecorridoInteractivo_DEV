e4d4232a31bf2dd6077f67fa634fefeb07e714ee
Isabella Urrego <isabellaurrego@gmail.com>
2026-09-12 22:04:20 -0500

    Prefab con información personalizable

--- ParqueDeLaConservacion/src/InformationController.ts
+++ ParqueDeLaConservacion/src/InformationController.ts
@@ -3,24 +3,33 @@ import * as ecs from '@8thwall/ecs'
 export const InformationController = ecs.registerComponent({
   name: 'InformationController',
 
-  schema: {
-    popup: ecs.eid,
-  },
-
+schema: {
+  popup: ecs.eid,
+  popupText: ecs.eid,
+  text: ecs.string,
+},
   stateMachine: ({world, eid, schemaAttribute}) => {
     ecs.defineState('default')
       .initial()
-      .listen(eid, ecs.input.UI_CLICK, () => {
-        const {popup} = schemaAttribute.get(eid)
+ .listen(eid, ecs.input.UI_CLICK, () => {
+  const {popup, popupText, text} = schemaAttribute.get(eid)
 
-        console.log('🔵 INFORMATION FUNCIONA')
-        console.log('🟡 POPUP RECIBIDO:', popup)
+  console.log('🔵 INFORMATION FUNCIONA')
 
-        if (popup) {
-          world.getEntity(popup).enable()
+  if (popupText) {
+    ecs.Ui.mutate(world, popupText, (cursor) => {
+      cursor.text = text
+      return false
+    })
 
-          console.log('🟢 POPUP ABIERTO')
-        }
-      })
+    console.log('📝 TEXTO CAMBIADO')
+  }
+
+  if (popup) {
+    world.getEntity(popup).enable()
+
+    console.log('🟢 POPUP ABIERTO')
+  }
+})
   },
 })
